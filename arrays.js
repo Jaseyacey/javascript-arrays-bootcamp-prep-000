45206d2f231b540899acb08d5b6c96e1c1edb81a
Jason Beedle <jason_beedle@hotmail.com>
2019-03-26 22:05:30 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,32 +1,38 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToBeginningOfArray(array, element){
-  return [element, ...array];
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
-  return(array)
+  return array
 }
 
 function addElementToEndOfArray(array, element) {
-  return[...array, element]
+  return [...array, element]
 }
 
-function destructivelyAddElementToEndOfArray(){
+function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element)
   return array
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-    array.shift(array)
+  array.shift()
+  array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
 }
 
 function removeElementFromEndOfArray(array) {
-  array.shift(array)
-}
-
-function removeElementFromEndOfArray() {
   return array.slice(0, array.length - 1)
 }
 
